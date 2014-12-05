c976f19784df04b80b0f12777782ba9fad77b05d
Kuba Niechciał <jakub.niechcial@netguru.co>
2014-12-05 16:24:20 +0100

    match controller edit

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,25 @@
 class MatchesController < ApplicationController
+  expose(:championship)
+  expose(:match, attributes: :match_params)
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if match.update_attributes(match_params)
+      flash[:notice] = "You updated match between #{match.team_1.name} and #{match.team_2.name}."
+      redirect_to championship
+    else
+      render 'edit'
+    end
+  end 
+
+
+  private
+    def match_params
+      params.require(:match).permit(:team_1_score, :team_2_score)
+    end
 end
