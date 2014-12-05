c2bb36c5f912242ca87b052123285b1302d84268
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2014-12-05 16:32:56 +0100

    connect service to controller

--- app/controllers/championships_controller.rb
+++ app/controllers/championships_controller.rb
@@ -1,2 +1,10 @@
 class ChampionshipsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    ChampionshipCreator.new(params[:championship][:user_ids]).create!
+    redirect_to root_path, notice: 'Championship has been created! Nie graj środkiem!'
+  end
 end
