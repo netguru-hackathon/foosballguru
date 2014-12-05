45ed01d55af6f8002ec99c9d43b73c46137b9c6f
Kuba Niechciał <jakub.niechcial@netguru.co>
2014-12-05 13:09:20 +0100

    validate netguru.pl domain

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,17 @@
 class CallbacksController < Devise::OmniauthCallbacksController
+  before_filter :check_ng_person
+
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
+  end
+
+  private
+
+  def check_ng_person
+    if request.env['omniauth.auth']['extra']['raw_info']['hd'] != 'netguru.pl'
+      flash[:error] = 'You are not netguru player, dude!'
+      redirect_to new_user_session_path
+    end
   end
 end
