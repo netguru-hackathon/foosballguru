7e380abbda8d714a74cf1ff15d48a008ec1d243e
Kuba Niechciał <jakub.niechcial@netguru.co>
2014-12-05 14:04:09 +0100

    user show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,3 @@
+class UsersController < ApplicationController
+  expose(:user)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
   root 'dashboard#index'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
