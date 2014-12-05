de67d06f47da56a7bee2fc854c072d99b785a579
Jacek Kubiak <jacek.kubiak@netguru.pl>
2014-12-05 16:54:15 +0100

    add championship controller

--- app/controllers/championships_controller.rb
+++ app/controllers/championships_controller.rb
@@ -1,10 +1,28 @@
 class ChampionshipsController < ApplicationController
-
   def new
+    @championship = Championship.new
+    @users = User.all
   end
 
   def create
-    ChampionshipCreator.new(params[:championship]).create!
-    redirect_to root_path, notice: 'Championship has been created! Nie graj środkiem!'
+    @championship = Championship.new(champ_params)
+    @users = User.all
+    if participant_count_valid? && @championship.save
+      ChampionshipCreator.new(params[:user_ids]).create!
+      redirect_to root_url, notice: 'Championship has been created! Nie graj środkiem!'
+    else
+      flash.now[:error] = "Nieprawidlowe dane"
+      render :new
+    end
+  end
+
+  private
+
+  def participant_count_valid?
+    champ_params[:participants_count].to_i == params[:user_ids].try(:count).to_i
+  end
+
+  def champ_params
+    params.require(:championship).permit(:participants_count)
   end
 end
